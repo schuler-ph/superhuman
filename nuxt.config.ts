f406d8e2d313dccf1f27b910c5a3708f0cbe1e89
Philipp Schuler <schulerp03@gmail.com>
2023-03-11 16:08:06 +0100

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -15,13 +15,47 @@ export default defineNuxtConfig({
         {
           name: "description",
           content: "Self-improvement and self-tracking app."
+        },
+        {
+          name: "msapplication-TileColor",
+          content: "#333333"
+        },
+        {
+          name: "theme-color",
+          content: "#ffffff"
         }
       ],
       link: [
         {
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/icon?family=Material+Icons"
-        }
+        },
+        {
+          rel: "apple-touch-icon",
+          sizes: "180x180",
+          href: "/apple-touch-icon.png"
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "32x32",
+          href: "/favicon-32x32.png"
+        },
+        {
+          rel: "icon",
+          type: "image/png",
+          sizes: "16x16",
+          href: "/favicon-16x16.png"
+        },
+        {
+          rel: "manifest",
+          href: "/site.webmanifest"
+        },
+        {
+          rel: "mask-icon",
+          href: "/safaro-pinned-tab.svg",
+          color: "#5bbad5"
+        },
       ]
     }
   },
