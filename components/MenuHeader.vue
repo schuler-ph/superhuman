9bb13911220b469d84a64dafafec70474d508c1e
Philipp Schuler <schulerp03@gmail.com>
2023-03-16 15:33:46 +0100

    not as much as expected

---
+++ components/MenuHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="sticky top-0 z-40 w-full bg-white dark:bg-slate-900 border-b border-slate-300 dark:border-slate-700">
+    <div class="max-w-screen-xl mx-auto">
+      <div class="px-4 lg:px-8 py-4 border-b lg:border-0 border-slate-300 dark:border-slate-700">
+        <div class="relative flex">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ layouts/defaultnew.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="text-slate-500 dark:text-slate-400 bg-white dark:bg-slate-900 antialiased">
+    
+  </div>
+</template>
