a39b51e2a772c8487773d23b1f67da196921fa54
Philipp Schuler <schulerp03@gmail.com>
2023-03-22 13:48:22 +0100

    nuxtlink x4

--- components/MenuHeader.vue
+++ components/MenuHeader.vue
@@ -3,17 +3,25 @@
     <div class="mx-auto max-w-screen-xl">
       <div class="border-b border-cBorder py-4 px-4 dark:border-cDarkBorder lg:border-0 lg:px-8">
         <div class="flex h-8">
-          <div class="flex w-8 items-center min-[350px]:w-auto">
+          <NuxtLink  to="/" class="flex w-8 items-center min-[350px]:w-auto">
             <img src="~/assets/svg/icon.svg" alt="superhuman Logo" class="h-8 w-8 min-[350px]:mr-3"/>
             <div class="hidden text-xl font-bold text-cTitle dark:text-cDarkTitle min-[350px]:block">superhuman</div>
-          </div>
+          </NuxtLink>
 
           <nav class="ml-auto hidden items-center font-bold text-cSmTitle dark:text-cDarkSmTitle lg:flex">
             <ul class="flex space-x-8">
-              <li class="navHoverLink">Movement</li>
-              <li class="navHoverLink">Nutrition</li>
-              <li class="navHoverLink">Memory</li>
-              <li class="navHoverLink">Habits</li>
+              <li class="navHoverLink">
+                <NuxtLink  to="/movement">Movement</NuxtLink>
+              </li>
+              <li class="navHoverLink">
+                <NuxtLink  to="/nutrition">Nutrition</NuxtLink>
+              </li>
+              <li class="navHoverLink">
+                <NuxtLink  to="/memory">Memory</NuxtLink>
+              </li>
+              <li class="navHoverLink">
+                <NuxtLink  to="/habits">Habits</NuxtLink>
+              </li>
             </ul>
             <ul class="ml-6 flex space-x-6 border-l border-cBorder pl-6 dark:border-cDarkBorder">
               <li class="navHoverLink">Sun</li>
@@ -34,5 +42,7 @@
 </script>
 
 <style scoped>
-
+.router-link-exact-active {
+  color: #2dd4be;
+}
 </style>
---
+++ pages/memory/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    Memory
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
