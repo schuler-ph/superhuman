cb704c07e3b6369dcf3015d90e1d6549a2f507be
Philipp Schuler <schulerp03@gmail.com>
2023-03-22 15:12:19 +0100

    icons to about

--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <h2>About</h2>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-      magna aliquyam</p>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
-      magna aliquyam</p>
-
-    <div>{{ data }}</div>
+  <div class="text-xl mx-auto">
+    Made with:
+  </div>
+  <div class="mx-auto p-4 flex space-x-4 border border-cBorder dark:border-cDarkBorder">
+    <!-- logos:visual-studio-code  logos:github-icon   logos:html-5 logos:css-3 logos:javascript logos:postman-icon
+    logos:nodejs-icon logos:typescript-icon logos:nodemon
+    -->
+    <Icon name="logos:vue" />
+    <Icon name="logos:nuxt-icon" />
+    <Icon name="logos:tailwindcss-icon" />
+    <Icon name="logos:netlify" />
+    <Icon name="simple-icons:express" />
+    <Icon name="logos:postgresql" />
+    <Icon name="logos:aws-rds" />
+    <Icon name="logos:aws" />
   </div>
 </template>
 
@@ -15,18 +22,11 @@
 //   method: "post",
 //   body: { age: 30 }
 // })
-const data = await useFetch("/api/currency/GBP")
+//const data = await useFetch("/api/currency/GBP")
 
 </script>
 
 <style scoped>
-h2 {
-  margin-bottom: 20px;
-  font-size: 36px;
-}
 
-p {
-  margin: 20px 0;
-}
 </style>
 
